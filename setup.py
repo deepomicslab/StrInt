824a2e9bf2ddb572ba772f87e989581d0e230854
wangjingwan <wangjingwan9684@gmail.com>
2025-04-08 23:03:17 +0800

    update ana_SCC

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup( name = 'pyStrint',
-version = '0.0.22',
+version = '0.0.20',
 description='Deciphering more accurate cell-cell interactions by modeling cells and their interactions',
 url='https://github.com/deepomicslab/StrInt',
 author='Jingwan WANG',
